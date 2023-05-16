9c63a68ccc40ec420b128059c3b98cb6c0d05362
Fernando Gonzalez <fernandogonzalez@MacBook-Pro-de-Fernando.local>
2023-05-15 20:16:00 -0400

    PPP-58 Pagina de Alumnos

--- app/src/pages/HomePageAlumno/HomePageAlumno.js
+++ app/src/pages/HomePageAlumno/HomePageAlumno.js
@@ -3,6 +3,7 @@ import './HomePageAlumno.css';
 import Formulario from '../../Formulario/Formulario.js'
 
 function HomePageAlumno() {
+  //se muestra el componente que se importa desde Formulario.js
   return (
     <Formulario/>
   );
