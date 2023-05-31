cdbd97e20f33f5ba293335cb8a054d7d54d6e5e0
FedeGomezM <fedgomez@alumnos.uai.cl>
2023-05-31 17:26:00 -0400

    PPP-93 Agregacion de Post y Get Request para agregar, eliminar y buscar alumnos pendientes

--- omega/server.js
+++ omega/server.js
@@ -2,30 +2,39 @@
 const express       = require('express'), 
       cors          = require('cors'), 
       path          = require('path'),
-      bodyParser    = require('body-parser');
-      //db_operation  = require('./db/db_files/db_operation.js');
+      db_operation  = require('./db_omega/db_operations.js');
 
-const API_PORT = process.env.PORT || 4000; //se corre api en el puerto 4000
+const API_PORT = process.env.PORT || 4000; //se corre api en el puerto 4000.
 const app = express();
 let client;
 let session;
-
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-app.use(express.static('public'));
 
-//Conexion con front
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+//request de traer alumnos en estado pendiente.
+app.post('/omega/bd/pendientes', async(req, res) => {
+  const RUNs = db_operation.arr_to_str(req.body);
+  const result = await db_operation.getAlumnosPendientes(RUNs, res);
+  
+  res.send(result.recordset);
 });
+
+//request de crear alumno
+app.post('/omega/bd/crear/alumno', async(req, res) => {
+  await db_operation.crearAlumno(req.body, res);
+});
+
+//request de eliminar alumno
+app.post('/omega/bd/eliminar/alumno', async(req, res) => {
+  await db_operation.removeAlumno(req.body, res);
+});
+
+//trae el pdf desde la carpeta de pdfs
+app.get('/omega/pdf/:filename', (req, res) => {
+  const folderPath = path.join(__dirname, 'pdfs');
+  const filePath = path.join(folderPath, req.params.filename);
+  res.sendFile(filePath);
+});
+
 
 app.listen(API_PORT, () => console.log(`Listening on Port ${API_PORT}`));
