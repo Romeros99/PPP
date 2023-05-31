51bc3dd26f0fbc12b938f365faaae531382e77ee
FedeGomezM <fedgomez@alumnos.uai.cl>
2023-05-31 17:20:34 -0400

    PPP-92 Cambios en la funcion para obtener los RUTs de los alumnos pendientes y eliminacion de funciones relacionadas a alumnos

--- backend/db/db_files/db_operation.js
+++ backend/db/db_files/db_operation.js
@@ -2,34 +2,15 @@ const config    = require('./db_config'),
       sql       = require('mssql')
 
 //Función que retorna todos los alumnos que estén en estado Pendiente de Verificación de Requisitos (Aquellos que tengan un registro de su reglamento pero que todavía no tengan su registro de detalle pasantía).
-const getAlumnosPendientes = async() => {
+const getRUNsPendientes = async() => {
   try {
     const pool = await sql.connect(config);
-    const alumnosPendientes = await pool.request()
-    .query(`SELECT A.* FROM Alumnos AS A 
-            JOIN Reglamentos AS R ON A.RUN_Alumno = R.RUN_Alumno
-            LEFT JOIN Detalle_Pasantia AS DP ON A.RUN_Alumno = DP.RUN_Alumno
+    const RUNsPendientes = await pool.request()
+    .query(`SELECT R.RUN_Alumno FROM Reglamentos AS R 
+            LEFT JOIN Detalle_Pasantia AS DP ON R.RUN_Alumno = DP.RUN_Alumno
             WHERE DP.RUN_Alumno IS NULL`);
 
-    return alumnosPendientes;
-  }
-  catch(error) {
-    console.log(error);
-    res.status(500).json({ error: 'ERROR: Error interno de servidor.' });
-    return error;
-  }
-}
-
-//Función para eliminar un alumno utilizando su RUT como método de filtración.
-const removeAlumno = async(Alumno, res) => {
-  try {
-    const pool = await sql.connect(config);
-    const deleteAlumno = await pool.request()
-    .query(`DELETE FROM Alumnos 
-            WHERE RUN_Alumno = '${Alumno.RUN_Alumno}'`);
-    
-    res.status(201).json({ message: 'Alumno eliminado exitosamente.' });
-    return;
+    return RUNsPendientes;
   }
   catch(error) {
     console.log(error);
@@ -51,62 +32,6 @@ const removeReglamento = async(Alumno, res) => {
   }
   catch(error) {
     console.log(error);
-    res.status(500).json({ error: 'ERROR: Error interno de servidor.' });
-    return error;
-  }
-}
-
-//Función para crear un alumno en la base de datos.
-const crearAlumno = async(Alumno, res) => {
-  try {
-    //Verifica que el RUT tenga un formato adecuado y, en caso de no tenerlo, envía un error como respuesta al Frontend.
-    const RUNRegExp = /^[0-9]{1,2}\.[0-9]{3}\.[0-9]{3}\-[0-9]$/;
-    if (!RUNRegExp.test(Alumno.RUN_Alumno)){
-      res.status(400).json({ error: 'ERROR: RUN inválido.' });
-      return;
-    }
-
-    //Verifica que los campos del alumno no estén vacíos y, en caso de ser vacíos, envía un error como respuesta al Frontend.
-    if (Alumno.Nombres.length === 0){
-      res.status(400).json({ error: 'ERROR: Por favor ingrese su nombre.' });
-      return;
-    }
-
-    if (Alumno.Apellidos.length === 0){
-      res.status(400).json({ error: 'ERROR: Por favor ingrese su apellido.' });
-      return;
-    }
-
-    if (Alumno.Mail_UAI.length === 0){
-      res.status(400).json({ error: 'ERROR: Por favor ingrese su mail universitario.' });
-      return;
-    }
-
-    if (Alumno.Mail_Personal.length === 0){
-      res.status(400).json({ error: 'ERROR: Por favor ingrese su mail personal.' });
-      return;
-    }
-
-    //Consulta si el alumno ya está en la base de datos, en caso de ser así, envía un error al Frontend.
-    const pool = await sql.connect(config);
-    const result = await pool.request()
-    .query(`SELECT * FROM Alumnos
-            WHERE RUN_Alumno = '${Alumno.RUN_Alumno}'`);
-    
-    if (result.recordset.length > 0) {
-      res.status(400).json({ error: 'ERROR: Alumno ya registrado.' });
-      return;
-    }
-
-    //En caso de que no se haya enviado un error, se realiza el Query para agregar al alumno a la base de datos.
-    const insertAlumno = await pool.request().query(`INSERT INTO Alumnos VALUES
-      ('${Alumno.RUN_Alumno}', '${Alumno.Nombres}', '${Alumno.Apellidos}', '${Alumno.Mail_UAI}', '${Alumno.Mail_Personal}')`);
-    
-    res.status(201).json({ message: 'Alumno registrado exitosamente.' });
-    return;
-  }
-  catch(error) {
-    console.error(error);
     res.status(500).json({ error: 'ERROR: Error interno de servidor.' });
     return error;
   }
@@ -148,10 +73,8 @@ const crearPasantia = async(Alumno, res) => {
 }
 
 module.exports = {
-  getAlumnosPendientes,
-  crearAlumno,
+  getRUNsPendientes,
   crearReglamento,
   crearPasantia,
-  removeAlumno,
   removeReglamento
 }
