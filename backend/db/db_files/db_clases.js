c3f85df488a4e5039f79d4f20bda9cf3ae59d526
Alejandro Romero Schmidt <aleromero@alumnos.uai.cl>
2023-06-01 12:57:30 -0400

    PPP-98 Se agrega clase Empresa

--- backend/db/db_files/db_clases.js
+++ backend/db/db_files/db_clases.js
@@ -33,4 +33,16 @@ class Detalle_Pasantia{
   }
 }
 
-module.exports = Alumno, Reglamento, Detalle_Pasantia;
+class Empresa{
+  constructor(RUN_Empresas, Nombre, Calle_Direccion, Numero_Direccion, Comuna_Direccion, Ciudad_Direccion, Rubro, Estado_Convenio){
+    this.RUN_Empresas = RUN_Empresas;
+    this.Nombre = Nombre;
+    this.Calle_Direccion = Calle_Direccion;
+    this.Numero_Direccion = Numero_Direccion;
+    this.Comuna_Direccion = Comuna_Direccion;
+    this.Ciudad_Direccion = Ciudad_Direccion;
+    this.Rubro = Rubro;
+    this.Estado_Convenio = Estado_Convenio;
+  }
+}
+module.exports = Alumno, Reglamento, Detalle_Pasantia, Empresa;
