3b8ff43e7f77e0224f309d05aa137909e57e2555
Alejandro Romero Schmidt <aleromero@alumnos.uai.cl>
2023-06-01 15:02:34 -0400

    PPP-97 Query Crear Empresa en BBDD

--- backend/db/db_files/db_operation.js
+++ backend/db/db_files/db_operation.js
@@ -71,6 +71,7 @@ const crearPasantia = async(Alumno, res) => {
     return error;
   }
 }
+
 const getEmpresas = async() => {
   try {
     const pool = await sql.connect(config);
@@ -86,10 +87,81 @@ const getEmpresas = async() => {
   }
 }
 
+//Funcion para crear empresa en BBDD
+const crearEmpresa = async(Empresa, res) => {
+  try {
+    //Verifica que el RUT tenga un formato adecuado y, en caso de no tenerlo, envía un error como respuesta al Frontend.
+    const RUNRegExp = /^[0-9]{1,2}\.[0-9]{3}\.[0-9]{3}\-[0-9]$/;
+    if (!RUNRegExp.test(Empresa.RUN_Empresas)){
+      res.status(400).json({ error: 'ERROR: RUN inválido.' });
+      return;
+    }
+
+    //Verifica que los campos del alumno no estén vacíos y, en caso de ser vacíos, envía un error como respuesta al Frontend.
+    if (Empresa.Nombre.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su nombre.' });
+      return;
+    }
+
+    if (Empresa.Calle_Direccion.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su calle.' });
+      return;
+    }
+
+    if (Empresa.Numero_Direccion.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su numero.' });
+      return;
+    }
+
+    if (Empresa.Comuna_Direccion.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su comuna.' });
+      return;
+    }
+
+    if (Empresa.Ciudad_Direccion.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su ciudad.' });
+      return;
+    }
+
+    if (Empresa.Rubro.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su rubro.' });
+      return;
+    }
+
+    if (Empresa.Estado_Convenio.length === 0){
+      res.status(400).json({ error: 'ERROR: Por favor ingrese su estado.' });
+      return;
+    }
+
+    //Consulta si el alumno ya está en la base de datos, en caso de ser así, envía un error al Frontend.
+    const pool = await sql.connect(config);
+    const result = await pool.request()
+    .query(`SELECT * FROM Empresas
+            WHERE RUN_Empresas = '${Empresa.RUN_Empresas}'`);
+
+    if (result.recordset.length > 0) {
+      res.status(400).json({ error: 'ERROR: Empresa ya registrada.' });
+      return;
+    }
+
+    //En caso de que no se haya enviado un error, se realiza el Query para agregar al alumno a la base de datos.
+    const insertEmpresa = await pool.request().query(`INSERT INTO Empresas VALUES
+      ('${Empresa.RUN_Empresas}', '${Empresa.Nombre}', '${Empresa.Calle_Direccion}', '${Empresa.Numero_Direccion}', '${Empresa.Comuna_Direccion}', '${Empresa.Ciudad_Direccion}', '${Empresa.Rubro}', '${Empresa.Estado_Convenio}')`);
+    
+    res.status(201).json({ message: 'Empresa registrada exitosamente.' });
+    return;
+  }
+  catch(error) {
+    console.error(error);
+    res.status(500).json({ error: 'ERROR: Error interno de servidor.' });
+    return error;
+  }
+} 
 module.exports = {
   getRUNsPendientes,
   crearReglamento,
   crearPasantia,
   removeReglamento,
-  getEmpresas
+  getEmpresas,
+  crearEmpresa
 }
