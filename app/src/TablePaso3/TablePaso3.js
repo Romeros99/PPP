0fbe9fdb26be453603b6208006405818325050d6
FedeGomezM <fedgomez@alumnos.uai.cl>
2023-06-16 19:01:06 -0400

    PPP-116 Cambios en la información registrada al apretar algún registro de pasantía y en lo que se muestra en la tabla

--- app/src/TablePaso3/TablePaso3.js
+++ app/src/TablePaso3/TablePaso3.js
@@ -3,20 +3,22 @@ import React from 'react';
 import './TablePaso3.css';
 
 //crea un tabla con las filas de la base de dato y largo dinamico segun los registros de alumnos
-const TablePaso3 = ({theadData, tbodyData, setDatos, setShowForm}) => {
+const TablePaso3 = ({theadData, tbodyData, datos, setDatos, setShowForm}) => {
   //Al apretar la fila de la pasantía, guarda los datos en la variable datos de HomePageAlumno
   const funcClick = (row) => {
-    setDatos({'RUN_Alumno': row.RUN_Alumno, 
-                'RUN_Empresas': row.RUN_Empresas, 
-                'Nombre': row.Nombre, 
-                'Calle_Direccion': row.Calle_Direccion,
-                'Numero_Direccion': row.Numero_Direccion,
-                'Comuna_Direccion': row.Comuna_Direccion,
-                'Ciudad_Direccion': row.Ciudad_Direccion,
-                'Rubro': row.Rubro,
-                'Nombres': row.Nombres,
-                'Apellidos': row.Apellidos,
-                'Mail': row.Mail
+    setDatos({'RUN_Empresa_Inicial': row.RUN_Empresas,
+              'ID_Supervisor': row.ID_Supervisor,
+              'RUN_Alumno': row.RUN_Alumno,
+              'RUN_Empresas': row.RUN_Empresas, 
+              'Nombre': row.Nombre, 
+              'Calle_Direccion': row.Calle_Direccion,
+              'Numero_Direccion': row.Numero_Direccion,
+              'Comuna_Direccion': row.Comuna_Direccion,
+              'Ciudad_Direccion': row.Ciudad_Direccion,
+              'Rubro': row.Rubro,
+              'Nombres': row.Nombres,
+              'Apellidos': row.Apellidos,
+              'Mail': row.Mail
               });
     //Activa el despliegue del formulario con la información de la pasantía
     setShowForm(true);
@@ -29,7 +31,10 @@ const TablePaso3 = ({theadData, tbodyData, setDatos, setShowForm}) => {
         <thead>
           <tr>
             {theadData.map(heading => {
-            return <th key={heading}>{heading}</th>
+            //No se muestra el ID_Supervisor en la tabla
+            if (heading !== 'ID_Supervisor'){
+              return <th key={heading}>{heading}</th>
+            }
             })}
           </tr>
         </thead>
@@ -37,7 +42,10 @@ const TablePaso3 = ({theadData, tbodyData, setDatos, setShowForm}) => {
           {tbodyData.map((row, index) => {
             return <tr key={index} onClick={() => funcClick(row)}>
               {theadData.map((key, index) => {
-                return <td key={row[key]}>{row[key]}</td>
+                //No se muestra el ID_Supervisor en la tabla
+                if (key !== 'ID_Supervisor'){
+                  return <td key={row[key]}>{row[key]}</td>
+                }
               })}
             </tr>;
           })}
